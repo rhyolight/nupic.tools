d9b017dec8fdd17afd055a2331abde2753b1e513
Matthew Taylor <rhyolight@gmail.com>
2014-11-21 10:14:36 -0800

    First working SSH on container

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,22 @@ FROM centos:centos7
 MAINTAINER Matthew Taylor <matt@numenta.org>
 ENV CREATION_DATE 2014-11-20
 
-RUN yum update
+RUN yum update -y
+RUN yum install git-core -y
 
-COPY . /src
+RUN mkdir /dependent-repos
+
+ENV NUPIC ${DEPENDENT_REPOS}/nupic
+ENV NUPIC_CORE ${DEPENDENT_REPOS}/nupic.core
+ENV NUPIC_RESEARCH /dependent-repos/nupic.research
+ENV NUPIC_REGRESSION ${DEPENDENT_REPOS}/nupic.regression
+ENV HTM_JAVA ${DEPENDENT_REPOS}/htm.java
+ENV NUMENTA_ORG ${DEPENDENT_REPOS}/numenta.org
+
+# Put dependent repositories into place
+RUN cd /dependent-repos; git clone https://github.com/numenta/nupic.research.git; git clone https://github.com/numenta/nupic.research.git; git clone https://github.com/numenta/nupic.git; git clone https://github.com/numenta/nupic.core.git; git clone https://github.com/numenta/nupic.regression.git; git clone https://github.com/numenta/htm.java.git; git clone https://github.com/numenta/numenta.org.git
+
+ADD . /src
 
 CMD /src/bin/prep-docker.sh
+# CMD bash
--- bin/prep-docker.sh
+++ bin/prep-docker.sh
@@ -2,6 +2,69 @@
 
 # start the ssh-agent in the background
 eval "$(ssh-agent -s)"
-ssh-add /keys/id_numenta-ci
-echo "Key added... testing github connection..."
-ssh -T git@github.com
+ssh-add /keys/docker
+# Manually set up authorized_keys
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cat /keys/docker.pub > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+if [ -z "$NUPIC" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC environment variable set to the numenta/nupic checkout."
+    exit 1
+fi
+
+if [ -z "$NUPIC_CORE" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC_CORE environment variable set to the numenta/nupic.core checkout."
+    exit 1
+fi
+
+if [ -z "$NUPIC_REGRESSION" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC_REGRESSION environment variable set to the numenta/nupic.regression checkout."
+    exit 1
+fi
+
+if [ -z "$NUPIC_RESEARCH" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUPIC_RESEARCH environment variable set to the numenta/nupic.research checkout."
+    exit 1
+fi
+
+if [ -z "$HTM_JAVA" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$HTM_JAVA environment variable set to the numenta/htm.java checkout."
+    exit 1
+fi
+
+if [ -z "$NUMENTA_ORG" ]; then
+    echo "In order for this script to run properly, you must have the "
+    echo "\$NUMENTA_ORG environment variable set to the numenta/numenta.org checkout."
+    exit 1
+fi
+
+cd ${NUPIC}
+git remote set-url git@github.com:numenta/nupic.git
+git pull origin master
+
+cd ${NUPIC_CORE}
+git remote set-url git@github.com:numenta/nupic.core.git
+git pull origin master
+
+cd ${NUPIC_REGRESSION}
+git remote set-url git@github.com:numenta/nupic.regression.git
+git pull origin master
+
+cd ${NUPIC_RESEARCH}
+git remote set-url origin git@github.com:numenta/nupic.research.git
+git pull origin master
+
+cd ${HTM_JAVA}
+git remote set-url git@github.com:numenta/htm.java.git
+git pull origin master
+
+cd ${NUMENTA_ORG}
+git remote set-url git@github.com:numenta/numenta.org.git
+git pull origin gh-pages
