ffe0187b6070be104b1133de46cf530d0c3d5c01
passiweinberger <passiweinberger@gmail.com>
2015-04-26 14:11:05 +0200

    Corrections

--- utils/mailman.js
+++ utils/mailman.js
@@ -1,19 +1,26 @@
-var postmark = require("postmark");
-var SERVER_KEY = process.env.POSTMARK_API_TOKEN;
-var SENDER = "help@numenta.org";
+var postmark = require("postmark")
+  , SERVER_KEY = process.env.POSTMARK_API_TOKEN
+  , SENDER = "help@numenta.org"
+  ;
+  
+if (! SERVER_KEY){
+    throw Error('The Postmark server-key is missing! '
+        + 'Please set it to send Emails using: ' +
+        '\nexport POSTMARK_API_TOKEN="<server_key>"');
+}
 
-function sendMail(To, Subject, Body){
+function sendMail(to, subject, body, callback){
 /*
-A simplified function using the Postmark Email-Client
-sing the specified sender mail-adress and the enviornment-variable POSTMARK_API_TOKEN
+A simple function using the Postmark Email-Client
+using the specified sender mail-adress and the enviornment-variable POSTMARK_API_TOKEN
+Set callback to check if your function call was successful.
 */
-var client = new postmark.Client(SERVER_KEY);
-client.sendEmail({
-    "From": SENDER, 
-    "To": To, 
-    "Subject": Subject, 
-    "TextBody": Body
-});
-return 0
-};
+    var client = new postmark.Client(SERVER_KEY);
+    client.sendEmail({
+        "From": SENDER
+      , "To": to
+      , "Subject": subject
+      , "TextBody": body
+    }, callback);
+}
 module.exports = sendMail;
