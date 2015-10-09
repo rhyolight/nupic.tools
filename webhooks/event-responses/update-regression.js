cf64c5a1204e21a2a0a995094445007ddc73a273
Matthew Taylor <matt@numenta.org>
2015-10-09 14:48:26 -0700

    Fixing SHA sent to updaters.

--- webhooks/event-responses/update-regression.js
+++ webhooks/event-responses/update-regression.js
@@ -2,10 +2,10 @@ var updateNupicModule = require('./update-nupic-module');
 
 module.exports = function(payload, callback) {
     updateNupicModule(
-        'numenta'
-      , 'nupic.regression'
-      , payload.after
-      , 'Automated update of nupic master sha to ' + payload.after + '.'
-      , callback
+        'numenta',
+        'nupic.regression',
+        payload.sha,
+        'Automated update of nupic master sha to ' + payload.sha + '.',
+        callback
     );
 };
--- webhooks/event-responses/update-research.js
+++ webhooks/event-responses/update-research.js
@@ -2,10 +2,10 @@ var updateNupicModule = require('./update-nupic-module');
 
 module.exports = function(payload, callback) {
     updateNupicModule(
-        'numenta'
-      , 'nupic.research'
-      , payload.after
-      , 'Automated update of nupic master sha to ' + payload.after + '.'
-      , callback
+        'numenta',
+        'nupic.research',
+        payload.sha,
+        'Automated update of nupic master sha to ' + payload.sha + '.',
+        callback
     );
 };
